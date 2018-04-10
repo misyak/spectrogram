6396d5dd71fb6ac1599e1416933e142f28248e3a
michal.morvay <michal.morvay@accenture.com>
2018-04-10 13:39:31 +0200

    handling of speaker segments + optimization + removed unused code

--- spectrogram.js
+++ spectrogram.js
@@ -51,11 +51,8 @@
 
     this._colors = colors;
     this._gfccInterval = options.interval;
+    this._colorSpectrumSteps = options.colorSpectrumSteps;
     
-    this._gfccSignals = this.parseGFCC(options.source.gfcc);
-
-    this._normGfcc = this.normalizeValues(this._gfccSignals, this.getMinValue(this._gfccSignals), this.getMaxValue(this._gfccSignals), options.colorSpectrumSteps);
-
     this._baseCanvasContext.fillStyle = this.getColor(0);
     this._baseCanvasContext.fillRect(0, 0, this._baseCanvas.width, this._baseCanvas.height);
 
@@ -63,28 +60,20 @@
     this._scaledCanvasContext.fillRect(0, 0, this._scaledCanvas.width, this._scaledCanvas.height);
   }
 
-  Spectrogram.prototype.drawGfccSpectrogram = function(gfccSignals, baseCanvasCtx, scaledCanvasCtx) {
+  Spectrogram.prototype.drawGfccSpectrogram = function(gfccSignals, baseCanvasCtx) {
     gfccSignals.forEach( signalsVector => {
-      this.draw(signalsVector, baseCanvasCtx, scaledCanvasCtx);
+      this.draw(signalsVector, baseCanvasCtx);
     });
   };
 
-  Spectrogram.prototype.draw = function(array, baseCanvasCtx, scaledCanvasCtx) {
+  Spectrogram.prototype.draw = function(array, baseCanvasCtx) {
       const baseCanvas = baseCanvasCtx.canvas;
       const baseWidth = baseCanvas.width;
       const baseHeight = baseCanvas.height;
 
-      const scaledCanvas = scaledCanvasCtx.canvas;
-      const scaledWidth = scaledCanvas.width;
-      const scaledHeight = scaledCanvas.height;
-
       const tempCanvasContext = baseCanvasCtx._tempContext;
       const tempCanvas = tempCanvasContext.canvas;
       tempCanvasContext.drawImage(baseCanvas, 0, 0, baseWidth, baseHeight);
-
-      const tempScaledCanvasContext = scaledCanvasCtx._tempContext;
-      const tempScaledCanvas = tempScaledCanvasContext.canvas;
-      tempScaledCanvasContext.drawImage(scaledCanvas, 0, 0, scaledWidth, scaledHeight);
 
       const intervalLength = this._gfccInterval[1] - this._gfccInterval[0]
       const pixels =  baseHeight/intervalLength;
@@ -92,33 +81,24 @@
       for (let i = 0; i < array.length; i++) {
         const value = array[i];
         baseCanvasCtx.fillStyle = this.getColor(value);
-        scaledCanvasCtx.fillStyle = this.getColor(value);
-        if (this._audioEnded) {
-          baseCanvasCtx.fillStyle = this.getColor(0);
-          scaledCanvasCtx.fillStyle = this.getColor(0);
-        }
-        baseCanvasCtx.fillRect(0, baseHeight - (i * pixels), 1, pixels);
-        scaledCanvasCtx.fillRect(0, scaledHeight - (i * pixels * 2), 2, pixels * 2);
+
+        baseCanvasCtx.fillRect(baseWidth - 1, baseHeight - (i * pixels), 1, pixels);
       }
 
-      baseCanvasCtx.translate(1, 0);
-      scaledCanvasCtx.translate(2, 0);
+      baseCanvasCtx.translate(-1, 0);
       // draw prev canvas before translation
       baseCanvasCtx.drawImage(tempCanvas, 0, 0, baseWidth, baseHeight, 0, 0, baseWidth, baseHeight);
-      scaledCanvasCtx.drawImage(tempScaledCanvas, 0, 0, scaledWidth, scaledHeight, 0, 0, scaledWidth, scaledHeight);
       // reset transformation matrix
       baseCanvasCtx.setTransform(1, 0, 0, 1, 0, 0);
-      scaledCanvasCtx.setTransform(1, 0, 0, 1, 0, 0);
 
       this._baseCanvasContext.drawImage(baseCanvas, 0, 0, baseWidth, baseHeight);
-      this._scaledCanvasContext.drawImage(scaledCanvas, 0, 0, scaledWidth, scaledHeight);
   };
 
   Spectrogram.prototype.drawScaled = function (sX, sY, sWidth, sHeight, dX, dY, dWidth, dHeight) {
     this._scaledCanvasContext.drawImage(this._baseCanvas, sX, sY, sWidth, sHeight, dX, dY, dWidth, dHeight);
   };
 
-  Spectrogram.prototype.createCanvases = function() {
+  Spectrogram.prototype.createCanvases = function(segments) {
     const baseCanvas = document.createElement('canvas');
     baseCanvas.width = this._baseCanvas.width;
     baseCanvas.height = this._baseCanvas.height;
@@ -128,23 +108,33 @@
     tempCanvas.width = baseCanvas.width;
     tempCanvas.height = baseCanvas.height;
 
-    const scaledCanvas = document.createElement('canvas');
-    scaledCanvas.width = this._scaledCanvas.width;
-    scaledCanvas.height = this._scaledCanvas.height;
-    const scaledCanvasContext = scaledCanvas.getContext('2d');
-
-    const scaledTempCanvas = document.createElement('canvas');
-    scaledTempCanvas.width = scaledCanvas.width;
-    scaledTempCanvas.height = scaledCanvas.height;
-
     baseCanvasContext._tempContext = tempCanvas.getContext('2d');
-    scaledCanvasContext._tempContext = scaledTempCanvas.getContext('2d');
 
-    this.drawGfccSpectrogram(this._normGfcc, baseCanvasContext, scaledCanvasContext);
+    this._reducedGfcc = this.createAndReduceGfccsArray(segments);
+    this._normGfcc = this.normalizeValues(this._reducedGfcc, this.getMinValue(this._reducedGfcc), this.getMaxValue(this._reducedGfcc), this._colorSpectrumSteps);    
+
+    this.drawGfccSpectrogram(this._normGfcc, baseCanvasContext);
+  };
+
+  Spectrogram.prototype.createAndReduceGfccsArray = function (segments){
+    const reducedGfccs = [];
+    Object.keys(segments).forEach((s)=>{
+      segments[s].gfccs.forEach((vector)=>{
+        const gfccVector = [];
+        vector.forEach((el, i) => {
+          // using just coefficiets that are defined in options
+          if (i >= this._gfccInterval[0] && i <= this._gfccInterval[1]) {
+            gfccVector.push(el);
+          }
+        });
+        reducedGfccs.push(gfccVector);
+      });
+    });
+    return reducedGfccs;
   };
 
   Spectrogram.prototype.clear = function() {
-    let baseCanvasContext, scaledCanvasContext, baseCanvas, scaledCanvas;
+    let baseCanvas, scaledCanvas;
 
     baseCanvas = this._baseCanvasContext.canvas;
     scaledCanvas = this._scaledCanvasContext.canvas;
@@ -183,23 +173,6 @@
     return color;
   };
 
-  Spectrogram.prototype.parseGFCC = function(gfccSource) {
-    const parsedValues = [];
-
-    gfccSource.forEach((vector)=>{
-      const gfccVector = [];
-      vector.forEach((el, i) => {
-        // using just coefficiets that are defined in options
-        if (i >= this._gfccInterval[0] && i <= this._gfccInterval[1]) {
-          const parsed = parseFloat(el);
-          gfccVector.push(parsed);
-        }
-      });
-      parsedValues.push(gfccVector);
-    });
-    return parsedValues;
-  };
-
   Spectrogram.prototype.getMaxValue = function(source) {
     let max = Math.max(...source[0]);
     source.forEach(vector =>{
@@ -232,7 +205,7 @@
     }
 
     const normalizedVectros = [];
-    source.forEach(vector => {
+    source.forEach( vector => {
       const normalizedValues = [];
       vector.forEach( value => {
         const normValue = Math.floor(value / oneColorRange);
